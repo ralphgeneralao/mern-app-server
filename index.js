04cb8ae1eb8759677926a94446b022b73c8a11ab
Ralph <ralph@dnamicro.com>
2021-06-12 07:07:10 +0800

    mern app with react

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+const postRoutes = require('./routes/posts')
+
+const app = express()
+dotenv.config()
+
+app.use(bodyParser.json({
+  limit: "30mb",
+  extended: true
+}))
+app.use(bodyParser.urlencoded({
+  limit: "30mb",
+  extended: true
+}))
+app.use(cors())
+
+app.use('/posts', postRoutes)
+
+app.get('/', (req,res) => {
+  res.send('Hello to MERN API')
+})
+
+const PORT = process.env.PORT || 5003
+
+mongoose.connect(process.env.CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
